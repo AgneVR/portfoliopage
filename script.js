b31792e6c1073e8957b828c1f9ec28fcc688d2fc
Agne V. R <agneverseckaite@gmail.com>
2021-12-01 11:51:29 +0200

    dark mode theme update

--- script.js
+++ script.js
@@ -5,13 +5,28 @@ tryDark.onclick = () => {
     let bodySelect = document.querySelector("body")
     bodySelect.classList.toggle("bgDark")
     darkCardBg()
-    darkBtn()
+    darkAtag()
     workCardBgChange()
     allAtagWhite()
+    contactBlockTheme()
+    darkGreyToLight()
+    darkBtn()
+    makeDarkInput()
+    makeDarkTextArea()
+    replaceOurClientsImg()
+    replaceFooterImg()
 }
+
 function darkCardBg() {
     let changeBg = document.querySelectorAll(".card-container")
     changeBg.forEach(el => {
+        el.classList.toggle("bgDarkBlocks")
+    })
+}
+
+function contactBlockTheme() {
+    let darkTheme = document.querySelectorAll(".darkTheme")
+    darkTheme.forEach(el => {
         el.classList.toggle("bgDarkBlocks")
     })
 }
@@ -23,6 +38,13 @@ function allAtagWhite() {
     })
 }
 
+function darkGreyToLight() {
+    let darkGrey = document.querySelectorAll(".dark-grey ")
+    darkGrey.forEach(el => {
+        el.classList.toggle("colorLight")
+    })
+}
+
 function workCardBgChange() {
     let workCard = document.querySelectorAll(".work-card")
     workCard.forEach(el => {
@@ -30,12 +52,54 @@ function workCardBgChange() {
     })
 }
 
-function darkBtn() {
-    let btnDark = document.querySelectorAll(".btnDark")
+function darkAtag() {
+    let btnDark = document.querySelectorAll("a.darkBtn")
     btnDark.forEach(btn => {
         btn.classList.toggle("btnDarkBg")
     })
 }
+
+function darkBtn() {
+    let buttonDark = document.querySelectorAll("button")
+    buttonDark.forEach(el => {
+        el.classList.toggle("btnDarkBgPink")
+    })
+}
+
+function makeDarkInput() {
+    let darktInp = document.querySelectorAll(".form-box form input")
+    darktInp.forEach(el => {
+        el.classList.toggle("darkInput")
+    })
+}
+
+function makeDarkTextArea() {
+    let darkText = document.querySelectorAll(".form-box form textarea")
+    darkText.forEach(el => {
+        el.classList.toggle("darkInput")
+    })
+}
+
+function replaceOurClientsImg() {
+    let ourClientsImg = document.querySelectorAll(".our-clients img")
+    ourClientsImg.forEach(el => {
+        if (el.src.match("https://inbio-react.netlify.app/static/f60ed605fd12e097aac6d9f93d7a2702/1e15b/brand-04.webp")) {
+            el.src = "https://inbio-react.netlify.app/static/315fa9d000ebf4a2995d16d13b17852e/e94a9/client1.webp"
+        } else {
+            el.src = "https://inbio-react.netlify.app/static/f60ed605fd12e097aac6d9f93d7a2702/1e15b/brand-04.webp"
+        }
+    })
+}
+
+function replaceFooterImg() {
+    let footerImg = document.querySelector("footer img")
+    if (footerImg.src.match("https://inbio-react.netlify.app/static/ef90c7f423dc084c800a1dc506c57d25/b791a/logo-dark.webp")) {
+        footerImg.src = "https://inbio-react.netlify.app/static/cbac6339da167a45b2deae57ed7b26cc/b791a/logo.webp"
+    } else {
+        footerImg.src = "https://inbio-react.netlify.app/static/ef90c7f423dc084c800a1dc506c57d25/b791a/logo-dark.webp"
+    }
+}
+
 
 if (allSideMenuBtn.length > 0) {
     allSideMenuBtn.forEach(btn => {
